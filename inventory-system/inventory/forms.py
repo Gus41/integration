2a812a942a28140f7cc64f409e8732946fdd446f
Gustavo <123191793+Gus41@users.noreply.github.com>
2026-06-20 18:02:39 -0300

    feat: initial inventory system implementation

---
+++ inventory-system/inventory/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from inventory.models import Item
+
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = "__all__"
--- inventory-system/inventory/views.py
+++ inventory-system/inventory/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from inventory.models import Item
+from inventory.forms import ItemForm
+
+def item_list(request):
+    items = Item.objects.order_by("name")
+
+    return render(
+        request,
+        "inventory/item_list.html",
+        {
+            "items": items,
+        },
+    )
+
+
+def item_create(request):
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("item_list")
+
+    else:
+        form = ItemForm()
+
+    return render(
+        request,
+        "inventory/item_form.html",
+        {
+            "form": form,
+            "title": "Create Item",
+        },
+    )
+
+
+def item_update(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+
+        if form.is_valid():
+            form.save()
+            return redirect("item_list")
+
+    else:
+        form = ItemForm(instance=item)
+
+    return render(
+        request,
+        "inventory/item_form.html",
+        {
+            "form": form,
+            "title": "Edit Item",
+        },
+    )
+
+
+def item_delete(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+
+    if request.method == "POST":
+        item.delete()
+        return redirect("item_list")
+
+    return render(
+        request,
+        "inventory/item_delete.html",
+        {
+            "item": item,
+        },
+    )
